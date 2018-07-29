eb83d42c715e2c3869db61ff331684e455a2dcc3
derrickque <quehan11@gmail.com>
2018-07-29 22:06:26 +0800

    优化代码结构

--- src/FloydInGraph.java
+++ src/FloydInGraph.java
@@ -2,37 +2,43 @@
  * Created by hanque on 2018/7/29.
  */
 
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class FloydInGraph {
-
-
-
-    public FloydInGraph(int size) {
-//        this.path = new int[size][size];
-        this.dist = new int[size][size];
-        this.size = size;
-    }
-
-    private static int size;
 
     private static int INF = -1;
 
-    //邻接矩阵
-    private static int[][] dist;
+    public static int findEnd(int i, int[][] matrix) {
+        int max = 0;
+        int end = 0;
+        for (int j = 0; j < matrix.length; j++) {
+            if(max < matrix[i][j]){
+                max = matrix[i][j];
+                end = j;
+            }
+        }
+        return end;
+    }
 
-//    private static int[][] path;
-
-    //最长距离
-    private static int distance = 0;
-
-    //结束节点
-    private static int end = 0;
+    public static int[][] floyd(int[][] matrix) {
+        for (int k = 0; k < matrix.length; k++) {
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix.length; j++) {
+                    //matrix[i][j]可能不可达，但matrix[i][k]和matrix[k][j]必须可达
+                    //i,j,k两两不相等，避免成环
+                    if (i != j && j!=k && i!=k
+                            && matrix[i][k] != INF
+                            && matrix[k][j] != INF
+                            && matrix[i][j] < matrix[i][k] + matrix[k][j]) {
+                        matrix[i][j] = matrix[i][k] + matrix[k][j];
+                    }
+                }
+            }
+        }
+        return matrix;
+    }
 
     public static void main(String[] args) {
-        FloydInGraph graph = new FloydInGraph(5);
+        //初始化邻接矩阵和起点
+        int begin = 0;
         int[][] matrix = {
                 {0, 10, 20, 50, INF},
                 {INF, 0, INF, INF, 20},
@@ -40,44 +46,13 @@ public class FloydInGraph {
                 {INF, INF, INF, 0, 10},
                 {INF, INF, INF, INF, 0},
         };
-        int begin = 0;
-        graph.initial(matrix);
-        graph.floyd();
-        graph.findPath(begin);
+
+        //floyd算法更新距离矩阵
+        matrix = FloydInGraph.floyd(matrix);
+        //i点开始的最长路径即为距离矩阵第i列除去matrix[i][i]的最大值
+        int end = FloydInGraph.findEnd(begin, matrix);
+        int distance = matrix[begin][end];
         System.out.println("start at "+ begin + " ,end at " + end + ",the longest path is:" + distance);
     }
 
-    public static void findPath(int i) {
-        distance = 0;
-        for (int j = 0; j < size; j++) {
-            if(distance < dist[i][j]){
-                distance = dist[i][j];
-                end = j;
-            }
-        }
-    }
-
-    public static void initial(int[][] matrix){
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                dist[i][j] = matrix[i][j];
-            }
-        }
-    }
-
-    public static void floyd() {
-        for (int k = 0; k < size; k++) {
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-                    if (i != j && j!=k && i!=k
-                            && dist[i][k] != INF
-                            && dist[k][j] != INF
-                            && dist[i][j] < dist[i][k] + dist[k][j]) {
-                        dist[i][j] = dist[i][k] + dist[k][j];
-                    }
-                }
-            }
-        }
-
-    }
 }
